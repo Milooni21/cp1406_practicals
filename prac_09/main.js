919664a4a5ba05d4f9d987854cd30d3bb5a031ac
MILOONI YAGNIK <Milooni21>
2021-05-22 11:48:41 +0530

    Add prac_09

---
+++ prac_09/main.js
@@ -0,0 +1,21 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("All fields must be filled out");
+      return false;
+    }
+  }
+function validateForm() {
+    var x = document.forms["myForm"]["email"].value;
+    if (x == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+}
+function validateForm() {
+    var x = document.forms["myForm"]["phone"].value;
+    if (x == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+}
